710cb9ef281f817c60aaac29b949532f7842dac1
Jana <133576874+Janasameh@users.noreply.github.com>
2025-08-17 23:06:32 +0300

    prettyifing menu

--- mini project/BankSimulator/Program.cs
+++ mini project/BankSimulator/Program.cs
@@ -1,35 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Account
 {
-    public string name;
+    public string Name;
     public int Balance;
 
-/// constructor////
-    public  Account( string name)
+    public Account(string name)
     {
-        this.name=name;
-        Balance=0;
+        this.Name = name;
+        Balance = 0;
     }
-    public void deposit( int Balance)
+
+    public void Deposit(int value)
     {
-        this.Balance+=Balance;
+        this.Balance += value;
     }
+
     public void Withdraw(int value)
     {
-        if(value <= this.Balance)
+        if (value <= this.Balance)
         {
-         this.Balance-=value;
+            this.Balance -= value;
         }
-        else if(value > this.Balance)
+        else
         {
-           Console.WriteLine("you don't have enough balance in your account for this transaction");
+            Console.WriteLine("You don't have enough balance in your account for this transaction");
         }
-         
     }
+
     public int Show()
     {
         return this.Balance;
+    }
+
+    public static void Menu(Account currentAccount)
+    {
+        Console.WriteLine("====== Bank Menu ======");
+        if (currentAccount == null)
+        {
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. Select Account");
+        }
+        else
+        {
+            Console.WriteLine($"Welcome, {currentAccount.Name}!");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. Withdraw");
+            Console.WriteLine("5. Show Balance");
+            Console.WriteLine("6. Exit");
+        }
     }
 }
 
@@ -37,44 +57,85 @@ class Program
 {
     static void Main()
     {
-      Console.WriteLine(" please write your name");
-      string inputName = Console.ReadLine();
-      Account ACC1 = new Account(inputName);
+        List<Account> accounts = new List<Account>();
+        Account currentAccount = null;
 
-while (true)
-{
-    
-Console.WriteLine("1. Deposit");
-Console.WriteLine("2. Withdraw");
-Console.WriteLine("3. Show Balance");
-Console.WriteLine("4. Exit");
+        while (true)
+        {
+            Account.Menu(currentAccount);
 
-    string choice=Console.ReadLine();
- 
-if (choice == "1")
-{
-Console.WriteLine(" please deposit money");
-int value = int.Parse(Console.ReadLine());
-        ACC1.deposit(value);
-          Console.WriteLine(ACC1.Show());
- }
-  
-else if (choice == "2")
- {
-Console.WriteLine(" please withraw money");
-int value = int.Parse(Console.ReadLine());
-        ACC1.Withdraw(value);
-          Console.WriteLine(ACC1.Show());
- }
-else if (choice == "3")
-{
-    Console.WriteLine(ACC1.Show());
-}
-else if (choice == "4")
-{
-    break; // exit loop
-}
-}
+            string choice = Console.ReadLine();
 
+            if (choice == "1")
+            {
+                Console.WriteLine("Please write your name");
+                string inputName = Console.ReadLine();
+                Account ACC1 = new Account(inputName);
+                currentAccount = ACC1;
+                accounts.Add(ACC1);
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Enter account name:");
+                string ACCname = Console.ReadLine();
+
+                foreach (Account account in accounts)
+                {
+                    if (account.Name == ACCname)
+                    {
+                        currentAccount = account;
+                        break;
+                    }
+                }
+
+                if (currentAccount == null)
+                {
+                    Console.WriteLine("Account not found");
+                }
+            }
+            else if (choice == "3")
+            {
+                if (currentAccount != null)
+                {
+                    Console.WriteLine("Please deposit money");
+                    int value = int.Parse(Console.ReadLine());
+                    currentAccount.Deposit(value);
+                    Console.WriteLine(currentAccount.Show());
+                }
+                else
+                {
+                    Console.WriteLine("Please select account");
+                }
+            }
+            else if (choice == "4")
+            {
+                if (currentAccount != null)
+                {
+                    Console.WriteLine("Please withdraw money");
+                    int value = int.Parse(Console.ReadLine());
+                    currentAccount.Withdraw(value);
+                    Console.WriteLine(currentAccount.Show());
+                }
+                else
+                {
+                    Console.WriteLine("Please select account");
+                }
+            }
+            else if (choice == "5")
+            {
+                if (currentAccount != null)
+                {
+                    Console.WriteLine(currentAccount.Show());
+                }
+                else
+                {
+                    Console.WriteLine("Please select account");
+                }
+            }
+            else if (choice == "6")
+            {
+                break;
+            }
+        }
     }
-}
+}  
